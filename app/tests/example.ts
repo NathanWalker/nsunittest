78755182b90f32e0b7b56282ed5426d52a0b8da7
Nathan Walker <walkerrunpdx@gmail.com>
2021-05-08 20:34:28 -0700

    chore: after adding custom packed unit test runner & webpack tgz's

--- app/tests/example.ts
+++ app/tests/example.ts
@@ -3,4 +3,9 @@ describe("A suite", function() {
   it("contains spec with an expectation", function() {
     expect(true).toBe(true);
   });
+
+  it("nsstring should be hello", function() {
+    const str = NSString.stringWithString('Hello');
+    expect(str).toBe(<any>'Hello');
+  });
 });
